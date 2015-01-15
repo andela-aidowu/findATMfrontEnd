6dabbcabd8e33b8df877f2df45b3277c55f27fe3
Abimbola Idowu <abimbola2010@gmail.com>
2015-01-15 23:24:39 +0100

    Initial Commit

---
+++ app/js/application.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('findatm', 
+  [
+    'ngResource', 
+    'ngCookies',  
+    'ngAnimate',  
+    'ngTouch',  
+    'ngSanitize',  
+    'ui.router', 
+    'ui.bootstrap', 
+    'ui.utils', 
+    'core', 
+    'users'
+  ])
