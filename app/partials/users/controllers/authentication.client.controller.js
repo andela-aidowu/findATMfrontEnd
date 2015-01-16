ee80a2fbd67d4eefdc7351515da9fffe86cf4538
Abimbola Idowu <abimbola2010@gmail.com>
2015-01-16 13:42:59 +0100

    User can now signup

--- app/partials/users/controllers/authentication.client.controller.js
+++ app/partials/users/controllers/authentication.client.controller.js
@@ -8,17 +8,28 @@ angular.module('users').controller('AuthenticationController', ['$scope', '$http
 		if ($scope.authentication.user) $location.path('/');
 
 		$scope.signup = function() {
-			$http.post('http://localhost:3000/auth/signup', $scope.credentials).success(function(response) {
-				// If successful we assign the response to the global user model
-				$scope.authentication.user = response;
+      var options = {
+        method: 'POST',
+        url: 'http://localhost:3000/auth/signup',
+        data: $.param($scope.credentials),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        }
+      };
+      $http(options).success(
+        function (data, status, headers, config) {
+        //do something
+        $scope.authentication.user = data;
 
-				// And redirect to the index page
-				$location.path('/');
-			}).error(function(response) {
-				$scope.error = response.message;
-			});
+        // And redirect to the index page
+        $location.path('/');
+        }
+      ).error(
+        function (data, status, headers, config) {
+        //do something
+        $scope.error = data.message;
+      });
 		};
-
 		$scope.signin = function() {
 			console.log($scope.credentials);
 			var options = {
