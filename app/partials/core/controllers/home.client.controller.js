bacff14ef5b428b3a92f1e08d9ef0a7b9bc82a77
Abimbola Idowu <abimbola2010@gmail.com>
2015-01-17 17:35:27 +0100

    basic styling for home page

--- app/partials/core/controllers/home.client.controller.js
+++ app/partials/core/controllers/home.client.controller.js
@@ -5,5 +5,65 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
+    $scope.atms = atm;
 	}
 ]);
+
+var atm = [
+  {
+    "bank": "Access Bank",
+    "Address": "No 2, Funho Street, Sabo-Yaba Lagos",
+    "State": "Lagos State",
+    "Estimate": "true",
+    "Distance": "23km"
+  },
+  {
+    "bank": "Access Bank",
+    "Address": "No 2, Funho Street, Sabo-Yaba Lagos",
+    "State": "Lagos State",
+    "Estimate": "true",
+    "Distance": "23km"
+  },
+  {
+    "bank": "Access Bank",
+    "Address": "No 2, Funho Street, Sabo-Yaba Lagos",
+    "State": "Lagos State",
+    "Estimate": "true",
+    "Distance": "23km"
+  },
+  {
+    "bank": "Access Bank",
+    "Address": "No 2, Funho Street, Sabo-Yaba Lagos",
+    "State": "Lagos State",
+    "Estimate": "true",
+    "Distance": "23km"
+  },
+  {
+    "bank": "Access Bank",
+    "Address": "No 2, Funho Street, Sabo-Yaba Lagos",
+    "State": "Lagos State",
+    "Estimate": "true",
+    "Distance": "23km"
+  },
+  {
+    "bank": "Access Bank",
+    "Address": "No 2, Funho Street, Sabo-Yaba Lagos",
+    "State": "Lagos State",
+    "Estimate": "true",
+    "Distance": "23km"
+  },
+  {
+    "bank": "Access Bank",
+    "Address": "No 2, Funho Street, Sabo-Yaba Lagos",
+    "State": "Lagos State",
+    "Estimate": "true",
+    "Distance": "23km"
+  },
+  {
+    "bank": "Access Bank",
+    "Address": "No 2, Funho Street, Sabo-Yaba Lagos",
+    "State": "Lagos State",
+    "Estimate": "true",
+    "Distance": "23km"
+  },
+];
