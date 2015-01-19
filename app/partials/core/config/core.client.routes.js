297c7f45509eefe2a28c1b03007052ded59a4ecc
Abimbola Idowu <abimbola2010@gmail.com>
2015-01-19 08:30:04 +0100

    changed url to fix bug with gh-pages

--- app/partials/core/config/core.client.routes.js
+++ app/partials/core/config/core.client.routes.js
@@ -1,17 +1,17 @@
 'use strict';
 
 // Setting up route
-angular.module('core', []).config(['$stateProvider', '$urlRouterProvider',
-	function($stateProvider, $urlRouterProvider) {
+angular.module('core', []).config(['$stateProvider', '$urlRouterProvider', 'appUrl', 
+	function($stateProvider, $urlRouterProvider, appUrl) {
 
 		// Home state routing
 		$stateProvider.
 		state('home', {
-			url: '/',
+			url: appUrl.githubUrl + '/',
 			templateUrl: 'app/partials/core/views/home.client.view.html'
 		}).
     state('add-atm', {
-      url: '/atm/add',
+      url: appUrl.githubUrl + '/atm/add',
       templateUrl: 'app/partials/core/views/add-atm.client.view.html'
     });
 	}
--- app/partials/users/config/users.client.routes.js
+++ app/partials/users/config/users.client.routes.js
@@ -1,44 +1,44 @@
 'use strict';
 
 // Setting up route
-angular.module('users').config(['$stateProvider',
-	function($stateProvider) {
+angular.module('users').config(['$stateProvider', 'appUrl',
+	function($stateProvider, appUrl) {
 		// Users state routing
 		$stateProvider.
 		state('profile', {
-			url: '/settings/profile',
+			url: appUrl.githubUrl + '/settings/profile',
 			templateUrl: 'app/partials/users/views/settings/edit-profile.client.view.html'
 		}).
 		state('password', {
-			url: '/settings/password',
+			url: appUrl.githubUrl +  '/settings/password',
 			templateUrl: 'app/partials/users/views/settings/change-password.client.view.html'
 		}).
 		state('accounts', {
-			url: '/settings/accounts',
+			url: appUrl.githubUrl + '/settings/accounts',
 			templateUrl: 'app/partials/users/views/settings/social-accounts.client.view.html'
 		}).
 		state('signup', {
-			url: '/signup',
+			url: appUrl.githubUrl + '/signup',
 			templateUrl: 'app/partials/users/views/authentication/signup.client.view.html'
 		}).
 		state('signin', {
-			url: '/signin',
+			url: appUrl.githubUrl + '/signin',
 			templateUrl: 'app/partials/users/views/authentication/signin.client.view.html'
 		}).
 		state('forgot', {
-			url: '/password/forgot',
+			url: appUrl.githubUrl + '/password/forgot',
 			templateUrl: 'app/partials/users/views/password/forgot-password.client.view.html'
 		}).
 		state('reset-invalid', {
-			url: '/password/reset/invalid',
+			url: appUrl.githubUrl + '/password/reset/invalid',
 			templateUrl: 'app/partials/users/views/password/reset-password-invalid.client.view.html'
 		}).
 		state('reset-success', {
-			url: '/password/reset/success',
+			url: appUrl.githubUrl + '/password/reset/success',
 			templateUrl: 'app/partials/users/views/password/reset-password-success.client.view.html'
 		}).
 		state('reset', {
-			url: '/password/reset/:token',
+			url: appUrl.githubUrl + '/password/reset/:token',
 			templateUrl: 'app/partials/users/views/password/reset-password.client.view.html'
 		});
 	}
