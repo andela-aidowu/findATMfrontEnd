3426c0bf88abc3e879d609bcefcbc4cb8a671436
Abimbola Idowu <abimbola2010@gmail.com>
2015-01-23 12:12:46 +0100

    version 0.0.2 ready for released

--- app/partials/core/services/url.client.value.js
+++ app/partials/core/services/url.client.value.js
@@ -1,6 +1,9 @@
 'use strict';
 
 angular.module('core').value('appUrl', {
-  baseUrl: 'http://localhost:3000/',
-  apiEndpoint: 'http://localhost:3000/api/v1/atms/'
+  baseUrl: 'https://findatm.herokuapp.com/',
+  apiEndpoint: 'https://findatm.herokuapp.com/api/v1/atms/'
 });
+
+//https://findatm.herokuapp.com/#!/
+//http://localhost:3000/
